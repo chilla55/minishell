87e9640be371e2cc8b6cd2ce10f330afd050e0d2
skorte <skorte@student.42wolfsburg.de>
2022-05-12 18:18:41 +0200

    fixed test.c

--- src/test.c
+++ src/test.c
@@ -6,7 +6,7 @@
 /*   By: skorte <skorte@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 09:22:09 by skorte            #+#    #+#             */
-/*   Updated: 2022/05/12 13:49:39 by skorte           ###   ########.fr       */
+/*   Updated: 2022/05/12 18:17:53 by skorte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,9 @@ int	init_exe(t_exe_list *exe_list, t_envp_list *envp_list);
 int main(int argc, char **argv, char **envp)
 {
 	t_exe_list *exe_list;
-	char	*command_1 = "cat src/minishell.c";
-//	char	*command_1 = "echo hello world";
+	t_envp_list *envp_list;
+//	char	*command_1 = "cat src/minishell.c";
+	char	*command_1 = "echo hello world";
 	char	*command_2 = "grep h";
 	char	*command_3 = "wc";
 
@@ -35,7 +36,8 @@ int main(int argc, char **argv, char **envp)
 	exe_list->next->next->argv = ft_split(command_3, ' ');
 	exe_list->next->next->command = ft_strdup(exe_list->next->next->argv[0]);
 	exe_list->next->next->next = NULL;
-	init_exe(exe_list, msh_create_envp_list(envp));
+	envp_list = msh_create_envp_list(envp);
+	init_exe(exe_list, envp_list);
 	msh_free_envp_list(envp_list);
 	return (0);
 }
