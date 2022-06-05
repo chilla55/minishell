b05b354735262416189457c46c6ba360ee30aef6
skorte <skorte@student.42wolfsburg.de>
2022-06-05 23:29:50 +0200

    redirections

--- src/builtins/msh_redirections.c
+++ src/builtins/msh_redirections.c
@@ -6,13 +6,15 @@
 /*   By: skorte <skorte@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 12:00:29 by skorte            #+#    #+#             */
-/*   Updated: 2022/05/21 18:11:20 by skorte           ###   ########.fr       */
+/*   Updated: 2022/06/02 23:15:52 by skorte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+static char	*read_input(char *delimiter);
 static char	*append_line(char *input, char *line);
+static char	*insert_envp(char *input, t_envp_list *envp_list);
 
 // *** > (append = 0) and >> (append = 1) ***
 
@@ -34,6 +36,7 @@ void	msh_redirect_to_file(char *path, int append)
 	{
 		write(fd_file, &ch, 1);
 	}
+	write(fd_file, &"\0", 1);
 	close(fd_file);
 	return ;
 }
@@ -58,18 +61,47 @@ void	msh_read_from_file(char *path)
 			if (read(fd_file, &ch, 1) > 0)
 				write(STDOUT_FILENO, &"\n", 1);
 		}
-		else
-			write(STDOUT_FILENO, &ch, 1);
+		write(STDOUT_FILENO, &ch, 1);
 	}
-	write(STDOUT_FILENO, &ch, 1);
 	close(fd_file);
-	close(STDOUT_FILENO);
 	return ;
 }
 
 // *** << (read input till delimiter)
 
-void	msh_read_input_till_delimiter(char *delimiter)
+void	msh_read_input_till_delimiter(char *delimiter,
+			char *silent, t_envp_list *envp_list)
+{
+	char	*input;
+	char 	*input_2;
+	int		stdout_envp;
+	int		stdout_backup;
+
+	if (!delimiter)
+	{
+		write(2, "Error: no delimiter given\n", 26);
+		return ;
+	}
+	stdout_backup = dup(STDOUT_FILENO);
+	stdout_envp = ft_atoi(msh_get_envp_value(envp_list, "STDOUT_BACKUP"));
+	dup2(stdout_envp, STDOUT_FILENO);
+	input = read_input(delimiter);
+	input_2 = insert_envp(input, envp_list);
+	dup2(stdout_backup, STDOUT_FILENO);
+	close(stdout_backup);
+	if (!silent)
+	{
+		write(STDOUT_FILENO, input_2, ft_strlen(input_2));
+		write(STDOUT_FILENO, &"\n", 1);
+	}
+	if (input)
+		free(input);
+	if (input_2)
+		free(input_2);
+	return ;
+}
+
+static char	*read_input(char *delimiter)
 {
 	char	*line;
 	char	*input;
@@ -79,29 +111,63 @@ void	msh_read_input_till_delimiter(char *delimiter)
 	while (1)
 	{
 		line = readline("> ");
-		if (!ft_strncmp(line, delimiter, ft_strlen(delimiter)))
+		if (!ft_strncmp(line, delimiter, ft_strlen(delimiter) + 1))
 			break ;
-		if (!input)
-			input = ft_strdup(line);
-		else
-			input = append_line(input, line);
+		input = append_line(input, line);
 		free(line);
 	}
-	write(STDOUT_FILENO, input, ft_strlen(input));
-	write(STDOUT_FILENO, &"\n", 1);
 	free(line);
-	if (input)
-		free(input);
-	return ;
+	return (input);
 }
 
 static char	*append_line(char *input, char *line)
 {
 	char	*temp;
 
-	temp = ft_strjoin_3 (input, "\n", line);
-	free(input);
-	input = ft_strdup(temp);
-	free(temp);
+	if (!input)
+		input = ft_strdup(line);
+	else
+	{
+		temp = ft_strjoin_3 (input, "\n", line);
+		free(input);
+		input = ft_strdup(temp);
+		free(temp);
+	}
 	return (input);
+}
+
+static char	*insert_envp(char *input, t_envp_list *envp_list)
+{
+	char	*ptr;
+	char	*envp;
+	char	*temp;
+	int		i;
+
+	envp = NULL;
+	temp = NULL;
+	i = 1;
+	if (!input)
+		return (NULL);
+	ptr = ft_strchr(input, '$');
+	if (!ptr)
+		return (input);
+	while (ptr)
+	{
+		temp = ft_substr(input, 0, ft_strlen(input) - ft_strlen(ptr));
+//		printf(" %s \n", temp);
+		while (ft_isalnum(ptr[i]))
+			i++;
+		envp = ft_substr(ptr, 1, i - 1);
+		printf("get %s :\n %s ***\n", envp, msh_get_envp_value(envp_list, envp));
+		temp = ft_strjoin(temp, msh_get_envp_value(envp_list, envp));
+//		printf(" %s \n", temp);
+		free(envp);
+		if (ptr + i)
+			input = ft_strjoin(temp, ptr + i);
+//		printf("***%s***\n", input);
+		ptr = ft_strchr(input, '$');
+	}
+	return (input);
+//	free(input);
+//	return (NULL);//msh_get_envp_value(envp_list, envp));
 }
