cdfe7bca50686c97ef288d241f42ee0b9ade9890
skorte <skorte@student.42wolfsburg.de>
2022-06-16 18:58:05 +0200

    export sorted

--- includes/libft/src/ft_strncmp.c
+++ includes/libft/src/ft_strncmp.c
@@ -29,6 +29,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
+	if (!s1 && !s2)
+		return (0);
+	else if (!s1)
+		return (- (unsigned char)s2[i]);
+	else if (!s2)
+		return ((unsigned char)s1[i]);
 	while (s1[i] == s2[i] && s1[i] != '\0' && i < (int)n - 1)
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
--- src/builtins/msh_export.c
+++ src/builtins/msh_export.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   msh_export.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agrotzsc <agrotzsc@student.42wolfsburg.    +#+  +:+       +#+        */
+/*   By: skorte <skorte@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 12:14:10 by agrotzsc          #+#    #+#             */
-/*   Updated: 2022/05/02 10:42:59 by agrotzsc         ###   ########.fr       */
+/*   Updated: 2022/06/16 18:57:00 by skorte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+static void	msh_print_envp_list_abc(t_envp_list *envp_list, char *last);
 
 void	free_split(char **strarr)
 {
@@ -44,7 +46,31 @@ void	msh_export_worker(char	**words, t_envp_list *envp_list, int i)
 void	msh_export(char	**words, t_envp_list *envp_list)
 {
 	if (!words[1])
-		msh_print_envp_list(envp_list);
+		msh_print_envp_list_abc(envp_list, NULL);
 	else
 		msh_export_worker(words, envp_list, 1);
+}
+
+static void	msh_print_envp_list_abc(t_envp_list *envp_list, char *last)
+{
+	t_envp_list	*ptr;
+	char		*next;
+
+	next = NULL;
+	ptr = envp_list;
+	while (ptr && ft_strncmp(ptr->name, last, ft_strlen(ptr->name)) <= 0)
+		ptr = ptr->next;
+	if (ptr)
+		next = ptr->name;
+	while (ptr)
+	{
+		if (ft_strncmp(ptr->name, last, ft_strlen(ptr->name)) > 0
+			&& ft_strncmp(ptr->name, next, ft_strlen(ptr->name)) < 0)
+			next = ptr->name;
+		ptr = ptr->next;
+	}
+	if (!next)
+		return ;
+	printf("declare -x %s=%s\n", next, msh_get_envp_value(envp_list, next));
+	msh_print_envp_list_abc(envp_list, next);
 }
