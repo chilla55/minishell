c6eaf19d417d0a498aa850f8a5c83b90af94993e
skorte <skorte@student.42wolfsburg.de>
2022-06-22 10:15:31 +0200

    try for echo\ in double quotes

--- src/parser/parse_5.c
+++ src/parser/parse_5.c
@@ -6,7 +6,7 @@
 /*   By: skorte <skorte@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 23:29:20 by skorte            #+#    #+#             */
-/*   Updated: 2022/06/21 23:38:58 by skorte           ###   ########.fr       */
+/*   Updated: 2022/06/22 00:53:23 by skorte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ char	**prepare_split_input(char *input)
 	return (input_split);
 }
 
+/*
 static char	*backslash_parse(char *input)
 {
 	int		quotes[2];
@@ -42,16 +43,60 @@ static char	*backslash_parse(char *input)
 	while (output[i])
 	{
 		test_quotes(output, i, &quotes[0], &quotes[1]);
-		if (output[i] == '\\' && ! quotes[0] && output[i + 1] != '\'')
+		if (output[i] == '\\' && output[i + 1] == '\\')
+			NULL;
+		else if (output[i] == '\\' && !quotes[0] &&
+			!quotes[1] && output[i + 1] != '\'')
 		{
 			output[i] = '\'';
 			quotes[0] = 1;
 			output = ft_strinsertchar(output, '\'', i + 2);
 		}
-		else if (output[i] == '\\' && ! quotes[0] && output[i + 1] == '\'')
+		else if (output[i] == '\\' && !quotes[0] && quotes[1])
+			output = ft_strdelchar(output, i);
+		else if (output[i] == '\\' && !quotes[0] && !quotes[1]
+			&& output[i + 1] == '\'')
 		{
 			output = ft_strdelchar(output, i);
 			output[i] = 127;
+		}
+		i++;
+	}
+	return (output);
+}
+*/
+
+static char	*backslash_parse(char *input)
+{
+	int		quotes[2];
+	int		i;
+	char	*output;
+
+	quotes[0] = 0;
+	quotes[1] = 0;
+	i = 0;
+	output = ft_strdup(input);
+	while (output[i])
+	{
+		test_quotes(output, i, &quotes[0], &quotes[1]);
+		if (output[i] == '\\' && !quotes[0] && quotes[1])
+		{
+			if (output[i + 1] == '\\')
+				output = ft_strdelchar(output, i);
+		}
+		else if (output[i] == '\\' && !quotes[0] && !quotes[1])
+		{
+			if (output[i + 1] != '\'')
+			{
+				output[i] = '\'';
+				quotes[0] = 1;
+				output = ft_strinsertchar(output, '\'', i + 2);
+			}
+			else
+			{
+				output = ft_strdelchar(output, i);
+				output[i] = 127;
+			}
 		}
 		i++;
 	}
@@ -71,7 +116,7 @@ static char	*dollar_parse(char *input)
 	while (output[i])
 	{
 		test_quotes(output, i, &quotes[0], &quotes[1]);
-		if (output[i] == '$' && ! quotes[0] && ft_strchr(" |\0", output[i + 1]))
+		if (output[i] == '$' && !quotes[0] && !quotes[1] && ft_strchr(" |\0", output[i + 1]))
 		{
 			if (i == 0 || (i > 0 && output[i - 1] != '$'))
 			{
